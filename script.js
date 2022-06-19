a6d2cfc1bd31d5294cdaf194bce275ddd1021794
Actatus <itsactatus@yahoo.com>
2022-06-18 20:10:06 -0700

    Add apiKey variables, prepare for fetch event.

---
+++ script.js
@@ -0,0 +1,11 @@
+const apiKey = config.APIKey;
+
+const searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', () => {
+    console.log("Search function will run");
+})
+
+function retrieveDataFromAPI(movieTitle){
+    //Fetch request will go here.
+};
