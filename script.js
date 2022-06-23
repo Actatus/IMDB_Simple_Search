f89ad5f57e0d54cf1666752e4c1ad864a55e7d5b
Actatus <itsactatus@yahoo.com>
2022-06-23 15:09:11 -0700

    Add fetch request w/ test value.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const apiKey = config.APIKey;
+const apiKey = config.APIKey; //pulls API key from seperate file.
 
 const searchButton = document.getElementById('searchButton');
 
 searchButton.addEventListener('click', () => {
-    console.log("Search function will run");
+    console.log(this.value);
+    console.log(retrieveDataFromAPI("Blade"));
 })
 
 function retrieveDataFromAPI(movieTitle){
     //Fetch request will go here.
+    // let search = fetch("https://imdb-api.com/en/API/Search/" + apiKey + "/" + movieTitle);
+    let requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch('https://imdb-api.com/en/API/Search/' + apiKey + '/' + movieTitle, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
 };
+
